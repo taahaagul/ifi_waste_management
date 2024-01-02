10bea74f1af26dcd5aa027ba58c9bebc24432e3f
TaahaaGul <tahagulll06@gmail.com>
2024-01-02 18:27:26 +0300

    SecurityConfiguration healing ./

--- src/main/java/com/taahaagul/ifiwastemanagement/config/SecurityConfiguration.java
+++ src/main/java/com/taahaagul/ifiwastemanagement/config/SecurityConfiguration.java
@@ -41,7 +41,7 @@ public class SecurityConfiguration {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .cors().and()
+                .cors(cors -> {})
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(req -> req
                         .requestMatchers(WHITE_LIST_URL)
